9a7946ae386a79c6193430878a9ae27a9121dfd6
Your Name <mohamedhamdy.o1997@gmail.com>
2021-08-01 23:05:33 +0200

    fix

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+//    'failed' => 'These credentials do not match our records.',
+    'failed' => 'خطأ في البريد الالكتروني او كلمه السر.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
--- resources/views/emails/account_data_for_new_users.blade.php
+++ resources/views/emails/account_data_for_new_users.blade.php
@@ -1,3 +1,19 @@
-<h1>name : {{$user->name}}</h1>
-<h1>email: {{$user->email}}</h1>
-<h1>password: {{$password}}</h1>
+<div style="text-align: right; direction: rtl">
+    <h1>تم اضافتك الي موقع المشاريب الخاص ب {{env('APP_NAME')}}</h1>
+    <br>
+    <h2>بيانات حسابك:</h2>
+    <h4>الاسم: {{$user->name}}</h4>
+    <h4>الدور:
+        @if($user->type == \App\Enums\AUserType::ADMIN)
+            ادمن
+        @elseif($user->type == \App\Enums\AUserType::EMPLOYEE)
+            موظف
+        @else
+            عامل مكتب
+        @endif
+    </h4>
+
+    <h4>البريد الالكتروني: {{$user->email}}</h4>
+    <h4>الرقم السري: {{$password}}</h4>
+
+</div>
